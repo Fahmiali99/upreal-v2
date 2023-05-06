4a3be3b23e732b08082af5d3e016410dd90d9fd9
Fahmi <fahmiali130499@gmail.com>
2023-05-06 12:27:12 +0700

    footer update

---
+++ src/utils/footer.tsx
@@ -0,0 +1,72 @@
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsPinterest,
+  BsDribbble,
+} from "react-icons/bs";
+
+export const legal = [
+  {
+    name: "Privacy Policy",
+    href: "#",
+  },
+  {
+    name: "Terms & Conditions",
+    href: "#",
+  },
+];
+
+export const product = [
+  {
+    name: "Logo Design",
+    href: "#",
+  },
+  {
+    name: "Flyer design",
+    href: "#",
+  },
+  {
+    name: "Poster design",
+    href: "#",
+  },
+  {
+    name: "Poster design",
+    href: "#",
+  },
+];
+
+export const adress = [
+  {
+    title: "Sumbersari, Jember",
+  },
+  {
+    title: "mekkipatria@gmail.com",
+  },
+  {
+    title: "+6281252188672",
+  },
+];
+
+export const sosmed = [
+  {
+    Icons: BsFacebook,
+    href: "#",
+  },
+  {
+    Icons: BsInstagram,
+    href: "#",
+  },
+  {
+    Icons: BsTwitter,
+    href: "#",
+  },
+  {
+    Icons: BsPinterest,
+    href: "#",
+  },
+  {
+    Icons: BsDribbble,
+    href: "#",
+  },
+];
